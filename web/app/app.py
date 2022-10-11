e4619e4d241d1b7842331a5d5d2f2cd72f3de709
etiennecorvee <etienne.corvee@caramail.com>
2022-10-11 22:33:15 +0200

    first changes....to keep or not to keep

--- web/app/app.py
+++ web/app/app.py
@@ -4,16 +4,25 @@ from flask import Flask, render_template, request, send_from_directory, jsonify,
 #from numpy import isin
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+OUTPUT_PATH = os.path.join(APP_ROOT, "results")
+if os.path.exists(OUTPUT_PATH) is False:
+    os.mkdir(OUTPUT_PATH)
+
+app.debug = True
 
 @app.route("/")
 def index():
+    app.logger.debug("/ endpoint: pid: " + str(os.getpid()))
+
     data = {"data": "Hello Camera3"}
     return jsonify(data)
 
 @app.route("/image", methods=['POST'])
 def image():
+    app.logger.debug("/image endpoint: pid: " + str(os.getpid()))
+
     # return render_template("upload.html")
     # return send_from_directory('/images','test.jpeg')
     # print(request)
@@ -42,11 +51,8 @@ def image():
     if isinstance(imagestr, str) is False:
         print("error decoding string")
     else:
-
-        #if os.path.exists("./output") is False:
-        #    os.mkdir("./output")
-
-        with open('./output/sample.png', 'wb') as f:
+        captured_image_path = os.path.join(OUTPUT_PATH, "sample.png") 
+        with open(captured_image_path, 'wb') as f:
             #f.write(base64.decodestring(imagestr.split(',')[1].encode()))
             f.write(base64.b64decode(imagestr.split(',')[1].encode()))
 
@@ -56,7 +62,11 @@ def image():
 
 @app.route("/camera", methods=['GET'])
 def upload():
-   return render_template("camera.html")
+
+    here pass a parameter url for the post image inside render template
+
+    app.logger.debug("/camera endpoint: pid: " + str(os.getpid()))
+    return render_template("camera.html")
 
 #if __name__ == "__main__":
 #    app.run(port=5000, debug=True)
