2e692f3b16dee96fbf9f7b4a2730b6fe5f8bb3a7
etiennecorvee <etienne.corvee@caramail.com>
2022-10-12 23:15:12 +0200

    first commit saving images instead of one, so the last one is available to be sent to future cpp client

--- web/app/app.py
+++ web/app/app.py
@@ -7,9 +7,40 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-#OUTPUT_PATH = os.path.join(APP_ROOT, "results")
-#if os.path.exists(OUTPUT_PATH) is False:
-#    os.mkdir(OUTPUT_PATH)
+class AppImage:
+    def __init__(self, filenameWithStamp: str):
+        get current timestamp without space here instead
+        self.filenameWithStamp = filenameWithStamp
+
+class BufferImages:
+    def __init__(self, maxLength: int, directory: str):
+        self.directory = directory
+        self.maxLength = maxLength
+        self.buffer = []
+        self.lastRecordedIndex = -1
+    def insert(self, inputImage: AppImage):
+        if self.lastRecordedIndex < 0:
+            self.buffer.append(inputImage)
+            self.lastRecordedIndex = 0
+            if len(self.buffer) != 1:
+                print("error inserting at 0")
+                return False
+        elif self.lastRecordedIndex == self.maxLength:
+here
+        self.lastRecordedIndex += 1 # -1->0, 9->10
+        if self.lastRecordedIndex == self.maxLength:
+            self.lastRecordedIndex = -1
+
+lastindex 
+-1 0
+0 1
+1 2
+
+OUTPUT_PATH = os.path.join(APP_ROOT, "output")
+if os.path.exists(OUTPUT_PATH) is False:
+    os.mkdir(OUTPUT_PATH)
+
+bufferImages = BufferImages(maxLength=10, directory=OUTPUT_PATH)
 
 #app.debug = True
 
@@ -20,46 +51,28 @@ def index():
     data = {"data": "Hello Camera3"}
     return jsonify(data)
 
+# this is called within the camera.html: var url = 'https://www.ecovision.ovh:81/image';
 @app.route("/image", methods=['POST'])
 def image():
-#    app.logger.debug("/image endpoint: pid: " + str(os.getpid()))
-
-    # return render_template("upload.html")
-    # return send_from_directory('/images','test.jpeg')
-    # print(request)
-
-    # DATA: b'{"email":"hello@user.com","response":{"name":"Tester"},"image":"data:image/png;base64,iVBORw0KGgoAAAANSUh
-    #print("DATA:", request.data)
-
     # bytes to string
-    # jsonstr: {"email":"hello@user.com","response":{"name":"Tester"},"image":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAUAAAA
     jsonstr = request.data.decode('utf8')
-
     # string to json
     data = json.loads(jsonstr)
-
-    # we could go further into beauty
-    #s = json.dumps(data, indent=4, sort_keys=True)
-    #print(s)
-
-
-    # ok
-    #print("email:", data["email"])
-    #print("response:", data["response"])
-    #print("image data:", data["image"])
+    # we could go further into beauty: s = json.dumps(data, indent=4, sort_keys=True)
 
     imagestr = data["image"]
     if isinstance(imagestr, str) is False:
         print("error decoding string")
     else:
-        #captured_image_path = os.path.join(OUTPUT_PATH, "sample.png") 
-        #with open(captured_image_path, 'wb') as f:
-        #if os.path.exists("./output") is False:
-        #    os.mkdir("./output")
 
-        with open('./output/sample.png', 'wb') as f:
+        filenameWithStamp = getCurrentTimestamp()
+
+        with open(os.path.join(bufferImages.directory, filenameWithStamp), 'wb') as f:
             #f.write(base64.decodestring(imagestr.split(',')[1].encode()))
             f.write(base64.b64decode(imagestr.split(',')[1].encode()))
+
+            appImage = AppImage(filenameWithStamp=filenameWithStamp)
+            bufferImages.insert(appImage)
 
     # unused but lets return something
     data = {"data": "Received image"}
@@ -67,14 +80,23 @@ def image():
 
 @app.route("/camera", methods=['GET'])
 def upload():
-
     #here pass a parameter url for the post image inside render template
-
     #app.logger.debug("/camera endpoint: pid: " + str(os.getpid()))
    return render_template("camera.html")
 
-#if __name__ == "__main__":
-#    app.run(port=5000, debug=True)
+# called by c++ client
+@app.route("/getlastimage", methods=["GET"])
+def image() -> str:
+   filename = os.path.join(get_test_dir(get_root_dir()), "data", "small.jpg")
+   with open( filename, mode="rb" ) as f:
+       imageContent = f.read().decode("iso-8859-1")
+       #(open(input_filename, "rb").read()).decode("iso-8859-1")
+       dict_out = {
+           "timestamp": "TODO_or_willbeinfilename",
+           "filename": "small.jpg",
+           "stream": imageContent
+       }
+       return dict_out
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
