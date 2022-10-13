674c04e5025443befbb8820dd2f6800683224c2f
etiennecorvee <etienne.corvee@caramail.com>
2022-10-13 22:58:16 +0200

    not working anymore

--- web/app/app.py
+++ web/app/app.py
@@ -1,29 +1,36 @@
 import os
 import base64
+import logging
 from datetime import datetime
 from flask import Flask, render_template, request, send_from_directory, jsonify, json
 #from numpy import isin
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.debug = True
+
+logging.basicConfig(level=logging.DEBUG)
+logging.warning('Start') 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 class AppImage:
     def __init__(self):
-        dt_obj = datetime.now()
-        dt_obj = ''.join(dt_obj.split())
-        print(dt_obj)
+        dt_obj = str(datetime.now())
+        dt_obj = dt_obj.replace(" ", "")
+        #print(dt_obj)
         self.filenameWithStamp = dt_obj + ".png"
-
+    def copyFrom(self, src):
+        self.filenameWithStamp = src.filenameWithStamp
+        
 class BufferImages:
     def __init__(self, maxLength: int, directory: str):
         self.directory = directory
         self.maxLength = maxLength
         self.buffer = []
         self.lastRecordedIndex = -1
-        for i in range(len(self.maxLength)):
-            appImage = AppImage("none");
-            self.buffer.append(appimage)
+        for i in range(self.maxLength):
+            appImage = AppImage();
+            self.buffer.append(appImage)
     def insert(self, inputImage: AppImage):
         index_nimage = self.lastRecordedIndex + 1# NextImageNotReadyYetForUploadAsNot
         if index_nimage >= self.maxLength:
@@ -32,7 +39,8 @@ class BufferImages:
         #now the image is in the buffer, fully saved, so it is available for the client => update index
         self.lastRecordedIndex = index_nimage;
 
-OUTPUT_PATH = os.path.join(APP_ROOT, "output")
+OUTPUT_PATH = os.path.join(APP_ROOT, "..", "..", "images")
+logging.debug("check output folder: "+str(OUTPUT_PATH))
 if os.path.exists(OUTPUT_PATH) is False:
     os.mkdir(OUTPUT_PATH)
 
@@ -50,6 +58,8 @@ def index():
 # this is called within the camera.html: var url = 'https://www.ecovision.ovh:81/image';
 @app.route("/image", methods=['POST'])
 def image():
+    #app.logger("/image")
+    logging.debug("/image")
     # bytes to string
     jsonstr = request.data.decode('utf8')
     # string to json
@@ -60,13 +70,14 @@ def image():
     if isinstance(imagestr, str) is False:
         print("error decoding string")
     else:
-
         appImage = AppImage()
         filename = appImage.filenameWithStamp
+        print("/image save ", filename)
         with open(os.path.join(bufferImages.directory, filename), 'wb') as f:
             #f.write(base64.decodestring(imagestr.split(',')[1].encode()))
             f.write(base64.b64decode(imagestr.split(',')[1].encode()))
             bufferImages.insert(appImage)
+            print("/image image ready at ", bufferImages.lastRecordedIndex)
 
     # unused but lets return something
     data = {"data": "Received image"}
@@ -92,7 +103,7 @@ def upload():
 
 # called by c++ client
 @app.route("/getlastimage", methods=["GET"])
-def image() -> str:
+def getlastimage():
    filename = os.path.join(get_test_dir(get_root_dir()), "data", "small.jpg")
    with open( filename, mode="rb" ) as f:
        imageContent = f.read().decode("iso-8859-1")
