e0e159b1e412bf05e216bf51fbe287b0875a2db8
etiennecorvee <etienne.corvee@caramail.com>
2022-11-01 22:51:55 +0100

    script launch: add kill of python processes before strating the; over afain

--- launch.sh
+++ launch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+pkill python*
+res=$(pgrep python*)
+if [ "$res" != "" ]; then
+    echo "failed to kill all python processes: "$res
+    exit 1
+fi
 source venv/bin/activate
 python web/app/file_watcher.py &
 python web/app/app.py
